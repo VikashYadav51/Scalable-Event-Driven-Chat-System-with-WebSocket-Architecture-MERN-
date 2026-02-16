8451a6a4c7670c4fbdf9f7fbdced15ef454618b2
Vikash <vikashyadav5131@gmail.com>
2026-02-16 19:38:12 +0530

    upload the files other systum

---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+import ApiError from './ApiErrror';
+
+cloudinary.config({ 
+  cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
+  api_key: 'CLOUDINARY_API_KEY', 
+  api_secret: 'CLOUDINARY_API_SECRET'
+});
+
+
+const uploadFilePath = async(localFilePath) =>{
+    try{
+        if(!localFilePath){
+            throw new ApiError(400, "File path is required ", localFilePath);
+            // return null;
+        }
+
+        const upload = await cloudinary.v2.uploader.upload( localFilePath, {
+            resource_type : "auto",
+            overwrite : true,
+        });
+
+        console.log(upload);
+        return upload;
+    }
+
+    catch{
+        if (localFilePath && fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath);
+        }
+
+        console.log("error while uploading file on cloudinary", error);
+        process.exit(1);
+    }
+} 
+
+export default uploadFilePath;
